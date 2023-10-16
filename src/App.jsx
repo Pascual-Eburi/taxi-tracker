d89213ca19e20825f83d29aa8e11cc5aeb5c8f93
Pascual Eburi <pascualeburi@gmail.com>
2023-10-16 03:08:11 +0200

    Add car icon to map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
+import { Car } from './components/Icons'
+import L from 'leaflet'
 
 export default function App () {
   const position = [51.505, -0.09]
+  const CarIcon = L.divIcon({
+    html: `${Car}`,
+    className: 'svg-icon'
+  })
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
+      <Marker position={position} icon={CarIcon} />
     </MapContainer>
   )
 }
